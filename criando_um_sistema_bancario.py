fe16565fd034b3a5a0b90c55a6de618734007261
ruanpablo <43484485+ruanpablo@users.noreply.github.com>
2023-03-27 22:17:35 -0300

    Update criando_um_sistema_bancario.py

--- criando_um_sistema_bancario.py
+++ criando_um_sistema_bancario.py
@@ -53,8 +53,8 @@ while True:
         if extrato == "":
             print("Não foram realizadas movimentações.")
         else:
-            print("\n==================== EXTRATO =============================")
-            print(f"\n\n\n{extrato} \n")
+            print("\n===================== EXTRATO =============================\n")
+            print(f"{extrato}")
             print(f"Saldo atual da conta: {saldo:.2f}")
             print("============================================================")
     
